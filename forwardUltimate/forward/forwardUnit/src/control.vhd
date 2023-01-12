b2e27088c7b822a52b1825d40e96f5b407936019
CorvoHyatt <corvohyatt@gmail.com>
2023-01-11 20:59:10 -0600

    Señales del control en 0 fixed

--- forwardUltimate/forward/forwardUnit/src/control.vhd
+++ forwardUltimate/forward/forwardUnit/src/control.vhd
@@ -33,20 +33,26 @@ begin
 	
 		--add 
 		if we = '1' then 
-				wb <='0';
-				read <='0';
-				write<='0';	
-				jal <= '0';
-				aluOP<='0';
-				aluSRC<='0';  	  
-				beq <='0';
-				bne<='0';
-				memtoreg<='0';	 
-				jalr<='0';
+			wb <='0';
+			read <='0';
+			write<='0';	
+			jal <= '0';
+			aluOP<='0';
+			aluSRC<='0';  	  
+			beq <='0';
+			bne<='0';
+			memtoreg<='0';	 
+			jalr<='0';
 		elsif(opcode ="0110011" ) then 
 			wb<='1';   
 			memtoreg<='1';
+			read <='0';
+			write<='0';	
+			jal <= '0';
 			aluSRC<='0';
+			beq <='0';
+			bne<='0';	 
+			jalr<='0';
 			if(func7="0000000") then
 			 	aluOP<='0';
 			else
@@ -57,6 +63,13 @@ begin
 			aluOP<='0';
 			wb<='1';
 			memtoreg<='1';
+			read <='0';
+			write<='0';	
+			jal <= '0';
+			aluSRC<='0';  	  
+			beq <='0';
+			bne<='0';	 
+			jalr<='0';
 		--ld
 		elsif (opcode = "0000011") then
 			aluOP <='0';	--suma
@@ -65,6 +78,10 @@ begin
 			write<='1';
 			aluSRC<='0';
 			memtoreg<='0';
+			jal <= '0';	  
+			beq <='0';
+			bne<='0';	 
+			jalr<='0';
 		--sd
 		elsif (opcode = "0100011") then	
 			aluOP <='0';	--suma
@@ -73,6 +90,10 @@ begin
 			aluSRC<='0';
 			write<='0';	
 			memtoreg<='0';
+			jal <= '0';  	  
+			beq <='0';
+			bne<='0';	 
+			jalr<='0';
 		--jal
 		elsif(opcode = "1101111") then 
 			aluOP <='0';	--suma
@@ -81,7 +102,10 @@ begin
 			aluSRC<='1';
 			write<='1';	
 			memtoreg<='0';
-			jal <= '1';
+			jal <= '1';	  	  
+			beq <='0';
+			bne<='0';
+			jalr<='0';
 		--beq 
 		elsif(opcode = "1100011" and func3 ="000")	 then
 			wb<= '0';
@@ -91,7 +115,9 @@ begin
 			write<='0';
 			jal<='0';	
 			beq <='1';
-			bne <='0';
+			bne <='0';	
+			memtoreg<='0';	 
+			jalr<='0';
 		--bne 
 		elsif(opcode = "1100011" and func3= "001") then
 			wb <= '0';
@@ -101,7 +127,9 @@ begin
 			write<='0';
 			jal<='0';  
 			beq <='0';
-			bne <='1'; 
+			bne <='1';
+			memtoreg<='0';	 
+			jalr<='0';
 		--jalr
 		elsif(opcode = "1100111") then 
 			aluOP<='0';
@@ -111,8 +139,9 @@ begin
 			write<='0';
 			memtoreg<='0';
 			jalr<='1'; 
-					
-
+			jal <= '0';	  
+			beq <='0';
+			bne<='0';
 		end if;	   
 	end process;				   
 end control;
